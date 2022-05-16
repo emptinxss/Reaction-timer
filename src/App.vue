fcb26c1d3e44de824d066fec48ef25a12274aa67
Miki Leardini <mikileardini96@gmail.com>
2022-05-16 20:07:46 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+    <h1>  MPT reaction timer </h1>
+    <button @click="start" :disabled="isPlaying">Start</button>
+
+     <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+
+     <Results v-if="showResults" :score="score"  />
+
+</template>
+
+
+<script>
+import Block from "./components/Block.vue"
+import Results from "./components/Results.vue"
+
+export default {
+  name: "App",
+  data() {
+    return{
+      isPlaying: false,
+      showResults: false,
+      delay: null,
+      score: null,
+    }
+  },
+  components: {
+    Block, Results
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+
+</script>
+
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  color: #FCEDDA;
+  font-weight: normal;
+  text-align: center;
+  
+}
+
+#app h1 {
+  font-size: 7rem;
+  font-weight: 700;
+  animation: 1s ease-out 0s 1 slideInFromTop;
+}
+
+button {
+
+  background: #201E21 ;
+  color:  #FCEDDA;
+  border: none;
+  border-radius: 4px;
+  font-size: 2rem;
+  letter-spacing: 2px;
+  padding: 10px;
+  cursor: pointer;
+  margin: 10px;
+  font-weight: bold;
+}
+button:hover {
+  background: #FCEDDA;
+  color: #201E21;
+
+}
+
+button[disabled] {
+  background: #201E21 ;
+  color:  #FCEDDA;
+  opacity: 0.4;
+  cursor: not-allowed;
+  
+}
+
+@media screen and (max-width: 405px){
+   html {
+     font-size: 40%;
+   }
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,66 @@
+<template>
+
+     <p> Reaction time: {{ score }} ms</p>
+     <p :class="rank"> <span> {{ rank }} </span></p>
+
+</template>
+
+<script>
+export default {
+    props: ["score"],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 200) {
+            this.rank = "Challenger"
+        } else if(this.score < 250) {
+            this.rank = "Diamond"
+        } else if (this.score < 400) {
+            this.rank = "Platinum"
+        }else if (this.score < 500) {
+            this.rank = "Gold"
+        } else if (this.score < 600) {
+            this.rank = "Silver"
+        } else if (this.score < 700) {
+            this.rank = "Bronze"
+        } else {
+            this.rank = "Trash"
+        }
+        
+    }
+}
+</script>
+
+<style>
+p {
+    font-size: 3rem
+}
+span {
+    font-weight: bold;
+}
+.Challenger {
+    color: greenyellow;
+}
+.Diamond {
+     color: blue;
+}
+.Platinum {
+    color: aqua;
+}
+.Gold {
+    color:gold;
+}
+.Silver {
+    color: silver;
+}
+.Bronze {
+    color: coral;
+}
+.Trash {
+    color: black;
+}
+
+</style>
